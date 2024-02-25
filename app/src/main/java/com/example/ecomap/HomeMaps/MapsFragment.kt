11bdbc87f0aa2dbb4b7600fd9532b0ca905d7c51
Riankai1073 <riancorci@gmail.com>
2024-02-25 04:30:17 -0800

    displayed stats for each marker

--- app/src/main/java/com/example/ecomap/HomeMaps/MapsFragment.kt
+++ app/src/main/java/com/example/ecomap/HomeMaps/MapsFragment.kt
@@ -1,9 +1,11 @@
 package com.example.ecomap.HomeMaps
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.ecomap.R
 import com.github.doyaaaaaken.kotlincsv.client.CsvReader
@@ -13,6 +15,7 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import java.io.BufferedReader
 import java.io.File
@@ -20,12 +23,14 @@ import java.io.InputStream
 import java.io.InputStreamReader
 
 
-data class WaterData(val long: String, val lat: String, val status: String)
+data class WaterData(val name: String, val status: String, val drinkable: String)
 
 class MapsFragment : Fragment(), OnMapReadyCallback {
 
     private var city: Array<String>? = null
     private lateinit var googleMap: GoogleMap
+
+    private lateinit var display: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,6 +44,8 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
         super.onViewCreated(view, savedInstanceState)
 
         city = arguments?.getStringArray(ARG_CITY)
+
+        display = view.findViewById(R.id.water_stats)
 
         val mapFragment =
             childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
@@ -59,6 +66,9 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
             val csvReader = CsvReader()
             val rows: List<List<String>> = csvReader.readAll(csvContent)
 
+            val markers: MutableList<Marker> = mutableListOf()
+            val waterList: MutableList<WaterData> = mutableListOf()
+
             gMap?.let { googleMap ->
                 this.googleMap = googleMap
                 city?.let { city ->
@@ -67,17 +77,39 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
                         if (location[12] != "Bloom Longitude") {
                             val california =
                                 LatLng(location[11].toDouble(), location[12].toDouble())
-                            val status = location[17]
-                            Log.d("Coords", location[12]+" "+location[11])
-                            googleMap.addMarker(
-                                MarkerOptions().position(california).title("$status")
+
+                            waterList.add(
+                                WaterData(
+                                location[6],
+                                location[17],
+                                location[25]
                             )
+                            )
+
+//                            val status = location[17]
+//                            val name = location[6]
+//                            val drinkable = location[25]
+
+                            Log.d("Coords", location[12]+" "+location[11])
+                            val marker = googleMap.addMarker(
+                                MarkerOptions().position(california).title("${waterList.last().name}")
+                            )
+                            markers.add(marker)
+
                             googleMap.moveCamera(
                                 CameraUpdateFactory.newLatLngZoom(
                                     california,
                                     zoomLevel
                                 )
                             )
+
+                            // Set up marker click listener
+                            googleMap.setOnMarkerClickListener { clickedMarker ->
+                                val index = markers.indexOf(clickedMarker)
+                                    // Inflate the other layout containing the TextView
+                                    display.text = "${waterList[index].name}\nIssue: ${waterList[index].status}\nDrinkable: ${waterList[index].drinkable}"
+                                true // Return true to consume the event
+                            }
                         }
                     }
 
